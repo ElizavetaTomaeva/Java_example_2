ae1c8560ae9d020a300ab07f96f48b701855629b
ElizavetaTomaeva <lizvladt@mail.ru>
2017-03-10 16:35:37 +0300

    Add files via upload

--- Music.java
+++ Music.java
@@ -1,4 +1,3 @@
-
 public class Music {
     private String year;
     private String title;
--- MusicView.java
+++ MusicView.java
@@ -1,6 +1,3 @@
-/**
- * Created by Liza on 04.02.2016.
- */
 public class MusicView {
     public void printInformation(String trackTitle, String trackYear,
                                  String trackAuthor){
